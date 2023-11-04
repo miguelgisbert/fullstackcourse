5f2814e5d6536606828097083f369f5c35960674
Miguel Gisbert <miguel.gisbert@bluecodesolutions.com>
2023-11-04 23:40:59 +0100

    Exercise 1.10

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -7,18 +7,32 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const Button = ({type}) => {
+      if (type === "good") {
+        return <button onClick={() => setGood(good + 1)}>good</button>
+      }
+      if (type === "neutral") {
+        return <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      }
+      if (type === "bad") {
+        return <button onClick={() => setBad(bad + 1)}>bad</button>
+      }
+  }
+
+  const StatisticLine = ({text, value}) => <p> {text} {value}</p>
+
   const Statistics = ({good, neutral, bad}) => {
     return (
       <>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
       {good !== 0 || neutral !== 0 || bad !== 0 ? 
       <div>
-        <p>all {good + neutral + bad}</p>
-        <p>average {(good - bad) / (good + neutral + bad)}</p>
-        <p>possitive {good * 100 / (good + neutral + bad)} %</p>
+        <StatisticLine text="all" value={good + neutral + bad} />
+        <StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} />
+        <StatisticLine text="possitive" value={good * 100 / (good + neutral + bad) + " %"} />
       </div> 
       :
       <div>
@@ -31,9 +45,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button type="good"/>
+      <Button type="neutral"/>
+      <Button type="bad"/>
       <Statistics good={good} neutral={neutral} bad={bad} />
       
     </div>
