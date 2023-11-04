f228f27884399f100227fcd2a933e97d34def3d1
Miguel Gisbert <miguel.gisbert@bluecodesolutions.com>
2023-11-04 23:29:57 +0100

    Fix for 1.8 and 1.9

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -11,6 +11,9 @@ const App = () => {
     return (
       <>
       <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
       {good !== 0 || neutral !== 0 || bad !== 0 ? 
       <div>
         <p>all {good + neutral + bad}</p>
@@ -31,10 +34,8 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
       <Statistics good={good} neutral={neutral} bad={bad} />
+      
     </div>
   )
 }
