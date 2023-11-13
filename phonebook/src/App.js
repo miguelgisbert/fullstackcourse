0abada11c40f6ae382b444353e43e90a76c09943
Miguel Gisbert <miguel.gisbert@bluecodesolutions.com>
2023-11-13 16:44:24 +0100

    Exercise 2.11

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456', key: 0 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', key: 1 },
-    { name: 'Dan Abramov', number: '12-43-234345', key: 2 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', key: 3 }
-  ]) 
+  
+  const [persons, setPersons] = useState([])
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ searchString, setSearchString ] = useState('')
