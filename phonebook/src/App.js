6fc9325e5f164708f2ec5e74aa46123f1d5fcc53
Miguel Gisbert <miguel.gisbert@bluecodesolutions.com>
2023-11-18 01:19:08 +0100

    Exercise 2.18

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -21,8 +21,22 @@ const App = () => {
 
   const addPerson = (e)=>{
     e.preventDefault()
-    if(persons.some(person => person.name === newName)){
-      alert(`${newName} is already added to phonebook`)
+    const person = persons.find(person => person.name === newName)
+    if(person){
+      if(window.confirm(`${newName} is already added to phonebook, replace the old number with the new one?`)){
+        const changedPerson = { ...person, name: newName, number: newNumber}
+        personService
+          .update(person.id, changedPerson)
+          .then(returnedPerson => {
+            setPersons(persons.map(p => p.id !== person.id ? p : returnedPerson))
+          })
+          .catch(error => {
+            alert(
+              `the person '${newName}' was already deleted from server`
+            )
+            setPersons(persons.filter(n => n.id !== person.id))
+          })
+      }
     }
     else{
       const person = {
