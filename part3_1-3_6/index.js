17355c36bbdeaa9696b9140bbe7834a8b72d2882
Miguel Gisbert <miguel.gisbert@bluecodesolutions.com>
2023-12-21 13:29:36 +0100

    Exercise 3.1

---
+++ part3_1-3_6/index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+      id: 1,
+      name: "Arto Hellas",
+      number: "040-123456",
+    },
+    {
+      id: 2,
+      name: "Ada Lovelace",
+      number: "39-44-5323523",
+    },
+    {
+      id: 3,
+      name: "Dan Abramov",
+      number: "12-43-234345",
+    },
+    {
+      id: 4,
+      name: "Mary Poppendick",
+      number: "39-23-6423122",
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.use(express.json())
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  console.log(persons)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+  person.id = maxId + 1
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+  const person = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
